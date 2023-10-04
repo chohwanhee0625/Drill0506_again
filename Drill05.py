ac4f5db6f6cfaa84076609ace4d35f1458023dcd
chohwanhee(lggram) <great1625@gmail.com>
2023-10-04 10:30:11 +0900

    캐릭터가 손 화살표를 따라감

--- Drill05.py
+++ Drill05.py
@@ -25,14 +25,17 @@ def handle_events():
 
 
 def reset_world():
-    global running, cx, cy, frame
-    global hx, hy
+    global running
+    global cx, cy, hx, hy, sx, sy
+    global frame, t
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
-
-    # hx, hy = TUK_WIDTH - 100, TUK_HEIGHT - 100
-    hx, hy = random.randint(0, TUK_WIDTH-1), random.randint(0, TUK_HEIGHT-1)
+    
+    sx, sy = cx, cy     # p1 : 시작점
+    #hx, hy = TUK_WIDTH - 100, TUK_HEIGHT - 100
+    hx, hy = random.randint(0, TUK_WIDTH-1), random.randint(0, TUK_HEIGHT-1)    # p2 : 끝점
+    t = 0.0
 
 
 def render_world():
@@ -44,8 +47,14 @@ def render_world():
 
 
 def update_world():
-    global frame
+    global frame, t
+    global cx, cy
     frame = (frame + 1) % 8
+
+    if t <= 1.0:
+        cx = (1-t)*sx + t*hx        # 시작점과 끝점을 1-t:t 의 비율로 섞은 위치
+        cy = (1-t)*sy + t*hy
+        t += 0.001
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
